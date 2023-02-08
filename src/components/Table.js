a56617ad72c01831b048c509a4454687c496753d
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-02-08 08:08:14 -0300

    req 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <tr>
+          <th scope="col">Descrição</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Método de pagamento</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Moeda</th>
+          <th scope="col">Câmbio utilizado</th>
+          <th scope="col">Valor convertido</th>
+          <th scope="col">Moeda de conversão</th>
+          <th scope="col">Editar/Excluir</th>
+        </tr>
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+
+});
+
+Table.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
